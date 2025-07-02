fb2fbd02789a15281ea9a4b6474cd6537b890ba7
meesalamahalakshmi23 <meesalamahalakshmi23@gmail.com>
2025-07-02 17:34:09 +0530

    Add files via upload

---
+++ problems on dictionary.py
@@ -0,0 +1,60 @@
+# sample_dict = {"name": "Kelly","age": 25, "salary": 8000, "city": "New york"}
+# sample_dict.pop("name","salary")
+# sample_dict.pop("salary")
+# print(sample_dict)
+
+# str="python developer"
+# res={}
+# for i in str:
+#     if i not in res:
+#         res[i]=1
+#     else:
+#         res[i]+=1
+# print(res)        
+
+# from collections import Counter
+# srt="python developer"
+# res=Counter(str)
+# print(res)
+
+# sample_dict = {"name": "Kelly","age": 25, "salary": 8000, "city": "New york"}
+# res={}
+# for i,j in sample_dict.items():
+#     res[j]=i
+# print(res)
+    
+# k={'a':1,'b':2,'c':3,'d':4}
+# res=sum(dict.values(k))
+# print(res)
+
+# res={
+#     "student1":{'name':'jana','roll':101,'marks':86},
+#     "student2": {'name':'mouni','roll':102,'marks':95},
+#     "student3":{'name':'maha','roll':103,'marks':75}}
+# print(res)
+
+# sample_dict = {"name": "Kelly","age": 25, "salary": 8000, "city": "New york"}
+# tuple_list=list(sample_dict.items())
+# print(tuple_list)
+
+# k=["python","developer","html","css"]
+# res={}
+# for i in k:
+#     res[i]=len(i)
+# print(res)
+
+# res={}
+# for i in range(1,6):
+#     if i%2==0:
+        
+#         res[i]="even"
+#     else:
+#         res[i]="odd"    
+# print(res)        
+
+
+# words = ["cat", "dog", "bat"]
+# res = {}
+# for word in words:
+#     res[word] = word[::-1]
+# print(res)
